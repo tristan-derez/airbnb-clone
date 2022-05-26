aa99ffac5f8b008b967afdbdd80d0bd4d8449431
Tristan Derez <92602528+dreyzu@users.noreply.github.com>
2022-05-27 00:02:43 +0200

    add badge component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,41 @@
-import { Flex, Box, Image, Text } from "@chakra-ui/react";
+import { Flex, Box, Image, Text, Badge } from "@chakra-ui/react";
 import { AiFillStar } from "react-icons/ai";
+import data from "../data/data.json";
 
 export const Card = () => {
     return (
         <Flex paddingY="49px" pl="37px">
-            <Box w="176px" className="card">
-                <Image src="images/katie-zaferes.png" alt="photo of Katie Zaferes" w="176px" borderRadius="9px" />
+            <Box w="176px" className="card" position="relative">
+                <Image src={`/images/${data.img}`} alt="photo of Katie Zaferes" w="176px" borderRadius="9px" />
+                <Badge
+                    fontSize="10px"
+                    position="absolute"
+                    top="6px"
+                    left="6px"
+                    zIndex={"3"}
+                    borderRadius="2px"
+                    py="4px"
+                    px="6px"
+                    fontFamily="Poppins"
+                    fontWeight="400"
+                >
+                    SOLD OUT
+                </Badge>
                 <Box pt="9px" display="flex" alignItems="center">
                     <AiFillStar color="#FE395C" size="15" />
                     <Text ml="2px" fontSize="12px" fontFamily="Poppins" pt="2px">
-                        5.0
+                        {data.rating}
                     </Text>
                     <Text color="#918E9B" pl="2px" fontSize="12px" fontFamily="Poppins" pt="2px">
-                        (6) - USA
+                        ({data.reviewCount}) - {data.country}
                     </Text>
                 </Box>
                 <Text fontSize="12px" fontFamily="Poppins" fontWeight="300" pt="8px">
-                    Life lessons with Katie Zaferes
+                    {data.title}
                 </Text>
                 <Box display="flex" pt="8px">
                     <Text fontSize="12px" fontWeight="600" fontFamily="Poppins">
-                        From $136
+                        From ${data.price}
                     </Text>
                     <Text fontSize="12px" fontWeight="300" fontFamily="Poppins">
                         &nbsp;/ person
