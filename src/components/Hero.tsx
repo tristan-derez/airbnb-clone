eafaba6faafa4548e41d1dacaafb6d2bbf38c9fd
Tristan Derez <92602528+dreyzu@users.noreply.github.com>
2022-05-27 00:03:03 +0200

    change style

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,13 +4,21 @@ export const Hero = () => {
     return (
         <Flex pt="26px" direction="column" fontFamily="Poppins">
             <Flex justifyContent="center">
-                <Image src="images/photo-grid.png" alt="banner" w="395px" h="232px" />
+                <Image src="images/photo-grid.png" alt="banner" w={["248px", "auto"]} h="232px" />
             </Flex>
-            <Flex pl="36px" direction="column">
-                <Heading as="h1" pt="32px" fontFamily="Poppins" fontWeight="600" fontSize="36px" lineHeight="39.6px">
+            <Flex direction="column" px="36px">
+                <Heading
+                    as="h1"
+                    pt="32px"
+                    fontFamily="Poppins"
+                    fontWeight="600"
+                    fontSize="36px"
+                    lineHeight="39.6px"
+                    w={["248px", "auto"]}
+                >
                     Online Experiences
                 </Heading>
-                <Text pt="16px" fontWeight={"light"} lineHeight="17.6px" w="325px">
+                <Text pt="16px" fontWeight={"light"} lineHeight="17.6px" w={["248px", "auto"]}>
                     Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
                 </Text>
             </Flex>
